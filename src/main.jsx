f51b6caf43f36ab421ea51055270940a919e5b7f
Aman Singh <amanaspirantsingh@gmail.com>
2025-06-22 14:34:20 +0530

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App.jsx';
 import Dashboard from './pages/dashboard.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
      
     {/* < Dashboard /> */}
-  </BrowserRouter>
+  </HashRouter>
 )
